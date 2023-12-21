ab8b93bba4cebe505b0eb497155ede8d0e599ad5
Ivan <IvanBWork@yandex.ru>
2023-12-21 19:38:18 +0300

    Created a methods Input and PrintArray

--- task/Program.cs
+++ task/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿string[] Input(string text)
+{
+    Console.Write(text);
+    int elements = Convert.ToInt32(Console.ReadLine());
+    string[] array = new string[elements];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Введите {i + 1} элемент массива: ");
+        array[i] = Console.ReadLine()!;
+    } 
+    return array;
+}
+
+
+
+void PrintArray(string[] array, string text)
+{
+    string result = string.Join(", ", array);
+    Console.WriteLine(text + result);
+}
+
+string[] array = Input("Введите кол-во элементов массива: ");
+Console.WriteLine();
+PrintArray(array, "Ваш массив: ");
